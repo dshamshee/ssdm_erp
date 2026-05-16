d4441f036d506f19cda5159a535ffaf1b45be30a
KYs <amitkys59@gmail.com>
2026-05-16 14:58:50 +0530

    initial commit

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
+
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export { prisma };
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
