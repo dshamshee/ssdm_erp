1cd53770de9c266a7852431b66b0cc42678903af
KYs <amitkys59@gmail.com>
2026-05-23 23:37:49 +0530

    garbadge changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,3 @@
-import { headers } from "next/headers";
-import { auth } from "@/lib/auth";
-
-export default async function Page() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  return (
-    <div>
-      {/* condition check session exist or not */}
-      {session?.user ? (
-        <div>
-          <p>user name: {session.user.name}</p>
-          <p>user email: {session.user.email}</p>
-          <p>role: {session.user.role}</p>
-          <a href="/department">go to department</a>
-        </div>
-      ) : (
-        <div>
-          <p>Session not exist</p>
-          <a href="/auth/student/signup">Student Signup</a>
-          <a href="/auth/admin/signup">Admin Signup</a>
-          <a href="/department">Department</a>
-        </div>
-      )}
-    </div>
-  );
-
-  // return (
-  //   <div>
-  //     <LoaderScreen message="User details is loading..." />
-  //     <ErrorDisplay />
-  //   </div>
-  // )
+export default function Page() {
+  return <div>hi there</div>;
 }
--- lib/db/schema/department.ts
+++ lib/db/schema/department.ts
@@ -1,127 +1,211 @@
-import { sql } from "drizzle-orm";
-import { integer, pgTable, varchar, timestamp, boolean, date, check } from "drizzle-orm/pg-core";
+import { relations, sql } from "drizzle-orm";
+import {
+  boolean,
+  check,
+  date,
+  integer,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-// Independent master table 
-export const academicSessionTable = pgTable('academic_session', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 30 }).notNull().unique(), // review is latter / I want to generate it automatically like 2026-2030
-    startDate: date().notNull(),
-    endDate: date().notNull(),
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
+// Independent master table
+export const academicSessionTable = pgTable("academic_session", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({
+    length: 30,
+  })
+    .notNull()
+    .unique(), // review is latter / I want to generate it automatically like 2026-2030
+  startDate: date().notNull(),
+  endDate: date().notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 });
 
-
-
-// Independent master table 
-export const departmentTable = pgTable('department', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    code: varchar({ length: 10 }).notNull().unique(),
-    name: varchar({ length: 30 }).notNull().unique(),
-    description: varchar({ length: 100 }).notNull(), 
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
+// Independent master table
+export const departmentTable = pgTable("department", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  code: varchar({
+    length: 10,
+  })
+    .notNull()
+    .unique(),
+  name: varchar({
+    length: 30,
+  })
+    .notNull()
+    .unique(),
+  description: varchar({
+    length: 100,
+  }).notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 });
 
-
-// Independent master table 
-export const subjectTable = pgTable('subject', {
+// Independent master table
+export const subjectTable = pgTable(
+  "subject",
+  {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    code: varchar({ length: 10 }).notNull().unique(),
-    name: varchar({ length: 100 }).notNull(), 
-    description: varchar({ length: 255 }).notNull(),
-    type: varchar({ length: 30 }).notNull().default('MJC'), 
+    code: varchar({
+      length: 10,
+    })
+      .notNull()
+      .unique(),
+    name: varchar({
+      length: 100,
+    }).notNull(),
+    description: varchar({
+      length: 255,
+    }).notNull(),
+    type: varchar({
+      length: 30,
+    })
+      .notNull()
+      .default("MJC"),
     hasPractical: boolean().notNull().default(false),
     practicalFee: integer().notNull().default(0),
     // courseId: integer().references(() => courseTable.id, { onDelete: 'cascade' }).notNull(),
     // semesterId: integer().references(() => semesterTable.id, { onDelete: 'set null' }),
     createdAt: timestamp().defaultNow().notNull(),
     updatedAt: timestamp().defaultNow().notNull(),
-},
-(table) => [
-    check('type_check', sql`${table.type} IN ('MJC', 'MIC', 'MDC', 'SEC', 'VAC')`),
-]);
-
-
+  },
+  (table) => [
+    check(
+      "type_check",
+      sql`${table.type} IN ('MJC', 'MIC', 'MDC', 'SEC', 'VAC')`,
+    ),
+  ],
+);
 
 //  Depends on Department
-export const courseTable = pgTable('course', {
+export const courseTable = pgTable(
+  "course",
+  {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 50 }).notNull().unique(),
-    code: varchar({ length: 10 }).notNull().unique(),
-    type: varchar({ length: 30 }).notNull().default('UG Regular'),
-    description: varchar({ length: 255 }).notNull(),
-    departmentId: integer().references(() => departmentTable.id, { onDelete: 'cascade' }).notNull(),
-    duration: integer().notNull().default(4), 
+    name: varchar({
+      length: 50,
+    })
+      .notNull()
+      .unique(),
+    code: varchar({
+      length: 10,
+    })
+      .notNull()
+      .unique(),
+    type: varchar({
+      length: 30,
+    })
+      .notNull()
+      .default("UG Regular"),
+    description: varchar({
+      length: 255,
+    }).notNull(),
+    departmentId: integer()
+      .references(() => departmentTable.id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
+    duration: integer().notNull().default(4),
     isActive: boolean().notNull().default(true),
     createdAt: timestamp().defaultNow().notNull(),
     updatedAt: timestamp().defaultNow().notNull(),
-},
-(table) => [
-    check('duration_check', sql`${table.duration} BETWEEN 2 AND 8`),
-    check('type_check', sql`${table.type} IN ('UG Regular', 'UG Part Time', 'UG Vocational', 'PG Regular', 'PG Part Time', 'PG Vocational')`),
-]);
+  },
+  (table) => [
+    check("duration_check", sql`${table.duration} BETWEEN 2 AND 8`),
+    check(
+      "type_check",
+      sql`${table.type} IN ('UG Regular', 'UG Part Time', 'UG Vocational', 'PG Regular', 'PG Part Time', 'PG Vocational')`,
+    ),
+  ],
+);
 
+export const courseSessionTable = pgTable("course_session", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  courseId: integer()
+    .references(() => courseTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  sessionId: integer()
+    .references(() => academicSessionTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
+});
 
-
-export const courseSessionTable = pgTable('course_session',{
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    courseId: integer().references(() => courseTable.id, { onDelete: 'cascade' }).notNull(),
-    sessionId: integer().references(() => academicSessionTable.id, { onDelete: 'cascade' }).notNull(),
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
-})
-
-
-export const batchTable = pgTable('batch', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    courseSessionId: integer().references(() => courseSessionTable.id, { onDelete: 'cascade' }).notNull(),
-    name: varchar({ length: 30 }).notNull(), // Try to generate it automatically.
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
-})
-
+export const batchTable = pgTable("batch", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  courseSessionId: integer()
+    .references(() => courseSessionTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  name: varchar({
+    length: 30,
+  }).notNull(), // Try to generate it automatically.
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
+});
 
 // Belongs to a Course
-export const semesterTable = pgTable('semester', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    courseSessionId: integer().references(() => courseSessionTable.id, { onDelete: 'cascade' }).notNull(),
-    name: varchar({ length: 30 }).notNull(), // review is latter / I want to generate it automatically like Semester I, Semester II, etc.
-    semesterNumber: integer().notNull(), // e.g., 1, 2, 3... up to (duration * 2)
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
+export const semesterTable = pgTable("semester", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  courseSessionId: integer()
+    .references(() => courseSessionTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  name: varchar({
+    length: 30,
+  }).notNull(), // review is latter / I want to generate it automatically like Semester I, Semester II, etc.
+  semesterNumber: integer().notNull(), // e.g., 1, 2, 3... up to (duration * 2)
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 });
 
-
-export const semesterSubjectTable = pgTable('semester_subject',{
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    semesterId: integer().references(() => semesterTable.id, { onDelete: 'cascade' }).notNull(),
-    subjectId: integer().references(() => subjectTable.id, { onDelete: 'cascade' }).notNull(),
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
+export const semesterSubjectTable = pgTable("semester_subject", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  semesterId: integer()
+    .references(() => semesterTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  subjectId: integer()
+    .references(() => subjectTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 });
 
-// points directly to the course 
-export const feeTable = pgTable('fee', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    // courseId: integer().references(() => courseTable.id, { onDelete: 'cascade' }).notNull().unique(), // Unique ensures 1:1 relationship
-    // sessionId: integer().references(() => sessionTable.id, { onDelete: 'cascade' }).notNull(), // flexible fee for different sessions
-    // courseSessionId: integer().references(() => courseSessionTable.id, { onDelete: 'cascade' }).notNull(),
-    semesterId: integer().references(() => semesterTable.id, { onDelete: 'cascade' }).notNull(),
-    institution: integer().notNull().default(0),
-    university: integer().notNull().default(0),
-    late: integer().notNull().default(0),
-    practical: integer().notNull().default(0),
-    cultural: integer().notNull().default(0),
-    sports: integer().notNull().default(0),
-    miscellaneous: integer().notNull().default(0), 
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
+// points directly to the course
+export const feeTable = pgTable("fee", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  // courseId: integer().references(() => courseTable.id, { onDelete: 'cascade' }).notNull().unique(), // Unique ensures 1:1 relationship
+  // sessionId: integer().references(() => sessionTable.id, { onDelete: 'cascade' }).notNull(), // flexible fee for different sessions
+  // courseSessionId: integer().references(() => courseSessionTable.id, { onDelete: 'cascade' }).notNull(),
+  semesterId: integer()
+    .references(() => semesterTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  institution: integer().notNull().default(0),
+  university: integer().notNull().default(0),
+  late: integer().notNull().default(0),
+  practical: integer().notNull().default(0),
+  cultural: integer().notNull().default(0),
+  sports: integer().notNull().default(0),
+  miscellaneous: integer().notNull().default(0),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 });
 
-
-
-// Something happed here 
+// Something happed here
 // update 2 here
 // update 3 here
 // Update 4 here / after reject change something to merge
