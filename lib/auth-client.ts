df43e8e8193d1e46ae85188ea16b4f96e82c9017
KYs <amitkys59@gmail.com>
2026-07-13 16:12:35 +0530

    client origin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,8 +3,6 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
 
